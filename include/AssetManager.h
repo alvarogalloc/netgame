e714ade36f1e556ac0d5f154ddaa4cd655d5d6e8
alvarogalloc <galloalvaro5@gmail.com>
2022-11-06 00:16:10 -0600

    Finish initial AssetManager

--- include/AssetManager.h
+++ include/AssetManager.h
@@ -1,35 +1,30 @@
 #pragma once
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-#include <unordered_map>
+#include <vector>
 
-namespace netgame
-{
-enum class AssetType : uint8_t
-{
-    Music,
-    Texture
-};
-template <typename T> struct Asset
-{
-    std::size_t id;
-    T asset;
+namespace netgame {
+
+enum class AssetType : uint8_t { Music, Texture };
+
+template <typename T> struct Asset : T {
+  constexpr Asset(std::size_t _id) { id = _id; }
+  std::size_t id;
 };
 
-class AssetManager
-{
+class AssetManager {
+private:
+  std::string m_asset_path{};
+  std::size_t m_last_asset_id{0};
+  std::vector<Asset<sf::Texture> *> m_textures{};
+  std::vector<Asset<sf::Music> *> m_tracks{};
 
-  private:
-    std::string m_asset_path{};
-    std::size_t m_last_asset_id{0};
-    std::vector<Asset<sf::Texture>> m_textures{};
-    std::vector<Asset<sf::Music>> m_tracks{};
-  public:
-    AssetManager(const std::string &assets_path);
-    ~AssetManager() noexcept;
-    // returns id of loaded asset
-    std::size_t load(AssetType type, std::string path);
-    void unload(std::size_t id);
-    template <typename T> Asset<T> &get(std::size_t id);
+public:
+  AssetManager(const std::string &assets_path) noexcept;
+  ~AssetManager() noexcept = default;
+  // returns id of loaded asset
+  std::size_t load(AssetType type, std::string path);
+  void unload(std::size_t id);
+  template <typename T> Asset<T> *get(std::size_t id);
 };
 } // namespace netgame
--- src/AssetManager.cpp
+++ src/AssetManager.cpp
@@ -2,69 +2,61 @@
 
 using namespace netgame;
 
-AssetManager::AssetManager(const std::string &assets_path)
-{
-    // append '/' if not provided
-    if (!assets_path.ends_with('/'))
-    {
-        m_asset_path = assets_path + '/';
-    }
+AssetManager::AssetManager(const std::string &assets_path) noexcept {
+  // append '/' if not provided
+  if (!assets_path.ends_with('/')) {
+    m_asset_path = assets_path + '/';
+  }
 }
 
-std::size_t AssetManager::load(AssetType type, std::string path)
-{
+std::size_t AssetManager::load(AssetType type, std::string path) {
 
-    // Remove path clash with m_asset_path
-    if (path.starts_with('/'))
-    {
-        path.erase(0);
-    }
-    auto filepath = m_asset_path + path;
+  // Remove path clash with m_asset_path
+  if (path.starts_with('/')) {
+    path.erase(0);
+  }
+  auto filepath = m_asset_path + path;
 
-    switch (type)
-    {
-    case AssetType::Music: {
-        m_tracks.push_back({++m_last_asset_id, {}});
-        if (m_tracks.back().asset.openFromFile(filepath))
-        {
-            throw std::runtime_error{"Error loading Track: " + filepath};
-        }
+  switch (type) {
+  case AssetType::Music: {
+    m_tracks.push_back(new Asset<sf::Music>{++m_last_asset_id});
+    if (!m_tracks.back()->openFromFile(filepath)) {
+      throw std::runtime_error{"Error loading Track: " + filepath};
     }
-    case AssetType::Texture: {
-        m_textures.push_back({++m_last_asset_id, {}});
-        if (m_textures.back().asset.loadFromFile(filepath))
-        {
-            throw std::runtime_error{"Error loading Image: " + filepath};
-        }
+    break;
+  }
+  case AssetType::Texture: {
+    m_textures.push_back(new Asset<sf::Texture>{++m_last_asset_id});
+    if (!m_textures.back()->loadFromFile(filepath)) {
+      throw std::runtime_error{"Error loading Image: " + filepath};
     }
-    }
-    return ++m_last_asset_id;
+    break;
+  }
+  }
+  return m_last_asset_id;
 }
 
-void AssetManager::unload(std::size_t id)
-{
-    auto id_match = [id](const auto &asset) { return asset.id == id; };
-    {
-        auto res = std::find_if(m_textures.begin(), m_textures.end(), id_match);
-        if (res != m_textures.end())
-            m_textures.erase(res);
-    }
-    {
-        auto res = std::find_if(m_tracks.begin(), m_tracks.end(), id_match);
-        if (res != m_tracks.end())
-            m_tracks.erase(res);
-    }
+void AssetManager::unload(std::size_t id) {
+  auto id_match = [id](const auto &asset) { return asset->id == id; };
+  {
+    auto res = std::find_if(m_textures.begin(), m_textures.end(), id_match);
+    if (res != m_textures.end())
+      m_textures.erase(res);
+  }
+  {
+    auto res = std::find_if(m_tracks.begin(), m_tracks.end(), id_match);
+    if (res != m_tracks.end())
+      m_tracks.erase(res);
+  }
 }
 
-template <> Asset<sf::Texture> &AssetManager::get(std::size_t id)
-{
-    auto id_match = [id](const auto &asset) { return asset.id == id; };
+template <> Asset<sf::Texture> *AssetManager::get(std::size_t id) {
+  auto id_match = [id](const auto &asset) { return asset->id == id; };
 
-    return *std::find_if(m_textures.begin(), m_textures.end(), id_match);
+  return *std::find_if(m_textures.begin(), m_textures.end(), id_match);
 }
-template <> Asset<sf::Music> &AssetManager::get(std::size_t id)
-{
-    auto id_match = [id](const auto &asset) { return asset.id == id; };
+template <> Asset<sf::Music> *AssetManager::get(std::size_t id) {
+  auto id_match = [id](const auto &asset) { return asset->id == id; };
 
-    return *std::find_if(m_tracks.begin(), m_tracks.end(), id_match);
+  return *std::find_if(m_tracks.begin(), m_tracks.end(), id_match);
 }
